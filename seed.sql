74874e47debe65992838e7c45f197136742a1895
andreaives <andreasue3@gmail.com>
2020-10-27 16:12:39 -0600

    pseudo coding & adding files needed

---
+++ seed.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS workforce_db;
+
+CREATE DATABASE workforce_db;
+
+USE workforce_db;
+
+CREATE TABLE departments (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(30)
+)
+
+CREATE TABLE roles (
+
+)
+
+-- * **role**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **title** -  VARCHAR(30) to hold role title
+--   * **salary** -  DECIMAL to hold role salary
+--   * **department_id** -  INT to hold reference to department role belongs to
+
+-- * **employee**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **first_name** - VARCHAR(30) to hold employee first name
+--   * **last_name** - VARCHAR(30) to hold employee last name
+--   * **role_id** - INT to hold reference to role employee has
+--   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
---
+++ tracker.js
@@ -0,0 +1,10 @@
+//require in inquirer & MySQl
+
+
+
+//build out inquirer with questions
+// will need to have access to the data { choice }
+
+//add departments, roles, employees
+//view departments, roles, employees
+//update employee roles
