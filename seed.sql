5291b965b53f06439f991ffa5e3a8dac21396545
andreaives <andreasue3@gmail.com>
2020-10-27 22:06:18 -0600

    finished switch statement console logging data matches

--- seed.sql
+++ seed.sql
@@ -10,20 +10,19 @@ CREATE TABLE departments (
 )
 
 CREATE TABLE roles (
-
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT
 )
 
--- * **role**:
+CREATE TABLE employee (
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT UNA
+)
 
---   * **id** - INT PRIMARY KEY
---   * **title** -  VARCHAR(30) to hold role title
---   * **salary** -  DECIMAL to hold role salary
---   * **department_id** -  INT to hold reference to department role belongs to
+INSERT INTO roles (title, salary)
 
--- * **employee**:
-
---   * **id** - INT PRIMARY KEY
---   * **first_name** - VARCHAR(30) to hold employee first name
---   * **last_name** - VARCHAR(30) to hold employee last name
---   * **role_id** - INT to hold reference to role employee has
---   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
--- tracker.js
+++ tracker.js
@@ -1,9 +1,73 @@
 //require in inquirer & MySQl
+const inquirer = require("inquirer");
+// var mysql = require("mysql");
 
+// //create Mysql connection
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "abcd1234",
+//   database: "workforce_db"
+// });
 
+// //once connected to Mysql the start questions function
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId + "\n");
+//   startQuestions();
+// });
 
 //build out inquirer with questions
 // will need to have access to the data { choice }
+startQuestions = () => {
+ inquirer.prompt({
+ 
+  type: 'list',
+  name: 'action',
+  messsage: 'What action would you like to take?',
+  choices: [
+   "Add a department.", "Add a role.", "Add an employee.", 
+   "View departments", "View roles", "View employees", 
+   "Update employees roles."
+  ] 
+}).then(function(data){
+
+// if (data.action === "Add a department."){
+//  console.log("Success!")
+// }else if(data)
+switch (data.action) {
+ case "Add a department.":
+  console.log("Success!")
+ break;
+ case "Add a role.":
+  console.log("Success!")
+ break;
+ case "Add an employee.":
+  console.log("Success!")
+ break;
+ case "View departments":
+  console.log("Success!")
+ break;
+ case "View roles":
+  console.log("Success!")
+ break;
+ case "View employees":
+  console.log("Success!")
+ break;
+ case "Update employees roles.":
+  console.log("Success!")
+ break;
+}
+
+
+
+})
+.catch(console.error)
+
+}
+
+startQuestions();
 
 //add departments, roles, employees
 //view departments, roles, employees
