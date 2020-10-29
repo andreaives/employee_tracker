88625de0f78cd2f4dc44327600f2b9c348bfd0a0
andreaives <andreasue3@gmail.com>
2020-10-28 21:48:35 -0600

    Finished tebles + Join code

--- seed.sql
+++ seed.sql
@@ -5,24 +5,37 @@ CREATE DATABASE workforce_db;
 USE workforce_db;
 
 CREATE TABLE departments (
- id INT AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(30)
-)
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30)
+);
 
 CREATE TABLE roles (
 id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
-salary DECIMAL,
-department_id INT
-)
+salary DECIMAL
+);
 
-CREATE TABLE employee (
+CREATE TABLE employees (
 id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
-manager_id INT UNA
-)
+department_id INT
+);
+INSERT INTO departments (name)
+VALUE ("Accounting"), ("Contractors"), ("Legal"), ("Sales"), ("C Suite");
 
 INSERT INTO roles (title, salary)
+VALUE ("Accountant", 125000), ("Contractor", 800), ("CFO", 190000), ("Legal Lead", 220000), ("Sales Executive", 130000);
+ 
+INSERT INTO employees (first_name,last_name, role_id, department_id)
+VALUE ("Elizabreth", "Bunt", 3, 3), ("Doug","Smolls", 5, 4), ("Chris", "Cow", 1, 1), ("Carrie", "Carrington", 4, 5);
 
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
+
+SELECT employees.first_name, employees.last_name, roles.title, roles.salary, departments.name
+FROM employees
+LEFT JOIN roles ON employees.role_id = roles.id
+LEFT JOIN departments ON employees.department_id = departments.id
--- tracker.js
+++ tracker.js
@@ -4,88 +4,119 @@ var mysql = require("mysql");
 
 //create Mysql connection
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "abcd1234",
-  database: "workforce_db"
+ host: "localhost",
+ port: 3306,
+ user: "root",
+ password: "abcd1234",
+ database: "workforce_db"
 });
 
 //once connected to Mysql the start questions function
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  startQuestions();
+connection.connect(function (err) {
+ if (err) throw err;
+ console.log("connected as id " + connection.threadId + "\n");
+ startQuestions();
 });
 
 //build out inquirer with questions
 // will need to have access to the data { choice }
 startQuestions = () => {
  inquirer.prompt({
- 
+
   type: 'list',
-  name: 'action',
   messsage: 'What action would you like to take?',
+  name: 'action',
   choices: [
-   "Add a department.", "Add a role.", "Add an employee.", 
-   "View departments", "View roles", "View employees", 
+   "Add a department.", "Add a role.", "Add an employee.",
+   "View departments", "View roles", "View employees",
    "Update employees roles."
-  ] 
-}).then(function(data){
+  ]
+ }).then(function (data) {
 
- //need to send user different responses for choices.
-switch (data.action) {
- case "Add a department.":
-  console.log("Success!")
- break;
- case "Add a role.":
-  console.log("Success!")
- break;
- case "Add an employee.":
-  console.log("Success!")
- break;
- case "View departments":
-  console.log("Success!")
- break;
- case "View roles":
-  viewRoles()
- break;
- case "View employees":
-  console.log("Success!")
- break;
- case "Update employees roles.":
-  console.log("Success!")
- break;
-}
+  //need to send user different responses for choices.
+  switch (data.action) {
+   case "Add a department.":
+    console.log("Success!")
+    break;
+   case "Add a role.":
+    console.log("Success!")
+    break;
+   case "Add an employee.":
+    addEmployee();
+    break;
+   case "View departments":
+    console.log("Success!")
+    break;
+   case "View roles":
+    viewRoles()
+    break;
+   case "View employees":
+    console.log("Success!")
+    break;
+   case "Update employees roles.":
+    console.log("Success!")
+    break;
+  }
 
-})
-.catch(console.error)
-
-}
-function addDepartment(){
+ })
+  .catch(console.error)
 
 }
-
-function addRole() {
+addDepartment = () => {
 
 }
-function addEmployee() {
 
+addRole = () => {
+ //grab all departments and ask what department it goes into
+}
+addEmployee = () => {
+ connection.query("SELECT * FROM roles", (function (err,results){
+ // query to get all roles
+ inquirer.prompt([
+  {
+   name: 'first_name',
+   message: 'What is the employees first name?' 
+ },{
+  name: 'last_name',
+  message: 'What is the employees last name?' 
+ },{
+  type: 'list',
+  name: 'role',
+  message: 'What is the employees role?',
+  choices: () => {
+   let roleChoice = [];
+   for (let role of results.title){
+    roleChoice.push(role)
+   }
+  }
+ }
+ ])
+
+}) //.then
+ // //grab each role and array then present that as the choices
+ // //choices map see 
+ // connection.query(("INSERT INTO employees"))
+ // //array.find
+ // inquirer.prompt
+ // var query = connecion.query(
+ //  "INSERT INTO employees SET ?",
+
+ )
 }
 //can probably make all 3 a dynamic function
-function viewDepartmetn(){
+viewDepartment = () => {
 
 }
-function viewRoles(){
-connection.query("SELECT * FROM roles", function (err, results){
-if (err) throw err;
-console.table(results)
-})
+viewRoles = () => {
+ connection.query("SELECT * FROM roles", function (err, results) {
+  if (err) throw err;
+  console.table(results)
+ })
 }
-function viewEmployees(){
+viewEmployees = () => {
 
 }
-function updateEmployees(){
+updateEmployees = () => {
 
 }
 
